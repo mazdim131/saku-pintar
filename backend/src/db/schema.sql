233e17ee82fb91762eb6cb143a9cf41c8ac77f5a
mazdim131 <syandanadimas7.@gmail.com>
2026-04-10 14:19:30 +0700

    update be

--- backend/src/db/schema.sql
+++ backend/src/db/schema.sql
@@ -6,7 +6,6 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     total_balance DECIMAL(15,2) DEFAULT 0.00,
-    `limit` DECIMAL(15,2) DEFAULT 0.00,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
